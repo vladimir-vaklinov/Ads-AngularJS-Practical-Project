dca55c44ab43b7b6556f127bf426895f762057e0
Vladimir Vaklinov <vladeto87@abv.bg>
2014-12-30 17:01:45 +0200

    adding login.html

--- ads-project/app/js/app.js
+++ ads-project/app/js/app.js
@@ -2,7 +2,10 @@ var app = angular.module('adsApp', ['ngRoute'])
 .config(function($routeProvider) {
 	$routeProvider.when('/register', {
 		templateUrl: 'templates/register.html'
-	})
+	});
+	$routeProvider.when('/login', {
+		templateUrl: 'templates/login.html'
+	});
 	$routeProvider.when('/ads', {
 		templateUrl: 'templates/all-ads.html'
 	})
