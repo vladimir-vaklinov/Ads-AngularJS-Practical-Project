221370d4ae81be7ef306e32b70076ac664b00405
Vladimir Vaklinov <vladeto87@abv.bg>
2014-12-31 12:27:37 +0200

    adding town and category filter (not finished)

--- ads-project/app/js/controllers/MainController.js
+++ ads-project/app/js/controllers/MainController.js
@@ -1,5 +1,11 @@
 app.controller('AdsController', function($scope, mainData) {
 	mainData.getAllAds(function(resp) {
 		$scope.data = resp;
-	})
+	});
+	mainData.getAllTowns(function(resp) {
+		$scope.towns = resp;
+	});
+	mainData.getAllCategories(function(resp) {
+		$scope.categories = resp;
+	});
 });
--- ads-project/app/js/services/mainPage.js
+++ ads-project/app/js/services/mainPage.js
@@ -8,6 +8,24 @@ app.factory('mainData', function($http, $log) {
 			.error(function(data, status, headers, config) {
 				$log.warn(data);
 			})	
+		},
+		getAllTowns: function(success) {
+			$http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/towns'})
+			.success(function(data, status, headers, config) {
+				success(data);
+			})
+			.error(function(data, status, headers, config) {
+				$log.warn(data);
+			})	
+		},
+		getAllCategories: function(success) {
+			$http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/categories'})
+			.success(function(data, status, headers, config) {
+				success(data);
+			})
+			.error(function(data, status, headers, config) {
+				$log.warn(data);
+			})	
 		}
 	}
 })
