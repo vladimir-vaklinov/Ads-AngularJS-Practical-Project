299f85ea77ebaf46eddfe013f689b8da2f57b8c5
Vladimir Vaklinov <vladeto87@abv.bg>
2015-01-08 00:30:18 +0200

    implementing displaying public ads

---
+++ ads-project/js/controllers/PublicAdsCtrl.js
@@ -0,0 +1,7 @@
+app.controller('PublicAdsCtrl', ['$scope', 'adsData', function($scope, adsData) {
+	adsData.getPublicAds()
+		.$promise
+		.then(function (data) {
+			$scope.adsData = data;
+		})
+}])
---
+++ ads-project/js/services/data/adsData.js
@@ -0,0 +1,32 @@
+ app.factory('adsData', ['$resource', 'baseServiceUrl', function($resource, baseServiceUrl) {
+ 	var resource = $resource(baseServiceUrl + 'ads:adId', {adId: '@id'}, {
+ 		update: { method: 'PUT' }
+ 	})
+
+ 	function getPublicAds() {
+ 		return resource.get();
+ 	}
+
+ 	function editAd (adId, ad) {
+ 		return resource.update({ id: adId }, ad);
+ 	}
+
+ 	function getAdById (adId) {
+ 		return resource.get({id: adId});
+ 	}
+
+ 	function addAd(ad) {
+ 		return resource.save(ad);  
+ 	}
+
+ 	function deleteAd (adId) {
+ 		return resource.delete({ id: adId });
+ 	}
+ 	return {
+ 		getPublicAds: getPublicAds,
+ 		edit: editAd,
+ 		getAdById: getAdById,
+ 		add: getAdById,
+ 		delete: deleteAd
+ 	}
+ }])
